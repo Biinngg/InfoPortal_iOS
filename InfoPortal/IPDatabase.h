b19bfa1d3d8979806f9d07549c1c68e6e681af0e
USTB <ustb@Stu-14.local>
2012-08-27 09:14:03 +0800

    initial commit.

---
+++ InfoPortal/IPDatabase.h
@@ -0,0 +1,13 @@
+//
+//  IPDatabase.h
+//  InfoPortal
+//
+//  Created by USTB on 12-8-27.
+//  Copyright (c) 2012年 USTB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IPDatabase : NSObject
+
+@end
---
+++ InfoPortal/IPDatabase.m
@@ -0,0 +1,13 @@
+//
+//  IPDatabase.m
+//  InfoPortal
+//
+//  Created by USTB on 12-8-27.
+//  Copyright (c) 2012年 USTB. All rights reserved.
+//
+
+#import "IPDatabase.h"
+
+@implementation IPDatabase
+
+@end
