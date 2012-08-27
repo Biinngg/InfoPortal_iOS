db2e592ca8963a418601f8c9f2217ea15cbfbb0f
USTB <ustb@Stu-14.local>
2012-08-27 08:40:03 +0800

    Initial Commit

---
+++ InfoPortal/InfoPortal.m
@@ -0,0 +1,13 @@
+//
+//  InfoPortal.m
+//  InfoPortal
+//
+//  Created by USTB on 12-8-27.
+//  Copyright (c) 2012年 USTB. All rights reserved.
+//
+
+#import "InfoPortal.h"
+
+@implementation InfoPortal
+
+@end
