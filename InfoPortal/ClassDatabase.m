401f343da23cd33eff33b85512f36c404dedc673
Candyroot <liubinging@gmail.com>
2012-08-29 22:00:08 +0800

    Wrote the implementation of getBuild and getFloor

--- InfoPortal/ClassDatabase.m
+++ InfoPortal/ClassDatabase.m
@@ -11,9 +11,33 @@
 @implementation ClassDatabase
 
 -(NSArray *) getBuild {
-    NSArray *array = [NSArray arrayWithObjects:@"name", nil];
+    NSMutableArray *result;
+    NSArray *array = [NSArray arrayWithObject:@"name"];
     IPDatabase *database = [IPDatabase alloc];
     [database open];
-    [database query:@"cla_build" :array :nil :@"floor_num DESC" :nil];}
+    int status = [database query:@"cla_build" :array :nil :@"floor_num DESC" :nil];
+    if(status == 1) {
+        NSArray *next = [database moveToNext];
+        while(next != nil) {
+            [result addObject:[next objectAtIndex:0]];
+            next = [database moveToNext];
+        }
+    }
+    return result;
+}
+
+-(int) getFloor:(NSString *)buildName {
+    int floorNum;
+    NSString *condition = @"name = ";
+    [condition stringByAppendingString:buildName];
+    NSArray *array = [NSArray arrayWithObject:@"floor_num"];
+    IPDatabase *database = [IPDatabase alloc];
+    [database open];
+    int status = [database query:@"cla_build":array:condition:nil:nil];
+    if(status == 1) {
+        NSArray *next = [database moveToNext];
+        floorNum = [next objectAtIndex:0];
+    }
+}
 
 @end
