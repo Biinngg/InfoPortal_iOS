6b8d401f82d3cc90b8bb969a58bd1fdee10cb38b
Candyroot <liubinging@gmail.com>
2012-08-31 11:29:23 +0800

    Fix some bug.

--- InfoPortal/IPDatabase.m
+++ InfoPortal/IPDatabase.m
@@ -18,7 +18,11 @@
     NSArray *paths =NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     NSString *documentsDirectory = [paths objectAtIndex:0];
     NSString *path = [documentsDirectory stringByAppendingPathComponent:@"infoportal.db" ];
-    return sqlite3_open([path UTF8String], &database);
+    if(sqlite3_open([path UTF8String], &database) == SQLITE_OK) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
 
 -(void) close {
@@ -29,29 +33,31 @@
     columnSize = [columns count];
     NSString *selectSql = @"select ";
     if(columns == nil)
-        selectSql = [selectSql stringByAppendingString:@"* "];
-    else
+        selectSql = [selectSql stringByAppendingString:@" * "];
+    else {
         for(NSString *column in columns) {
             selectSql = [selectSql stringByAppendingString:column];
-            selectSql = [selectSql stringByAppendingString:@" "];
+            selectSql = [selectSql stringByAppendingString:@", "];
         }
-    selectSql = [selectSql stringByAppendingString:@"from "];
+        selectSql = [selectSql substringToIndex:[selectSql length] - 2];
+    }
+    selectSql = [selectSql stringByAppendingString:@" from "];
     selectSql = [selectSql stringByAppendingString:table];
     if(selection != nil) {
-        selectSql = [selectSql stringByAppendingString:@"where "];
+        selectSql = [selectSql stringByAppendingString:@" where "];
         selectSql = [selectSql stringByAppendingString:selection];
     }
     if(orderBy != nil) {
-        selectSql = [selectSql stringByAppendingString:@"order by "];
+        selectSql = [selectSql stringByAppendingString:@" order by "];
         selectSql = [selectSql stringByAppendingString:orderBy];
     }
     if(limit != nil) {
-        selectSql = [selectSql stringByAppendingString:@"limit "];
+        selectSql = [selectSql stringByAppendingString:@" limit "];
         selectSql = [selectSql stringByAppendingString:limit];
     }
     selectSql = [selectSql stringByAppendingString:@";"];
+    NSLog(@"selectSql = %@", selectSql);
     const char *sqlSentence = [selectSql UTF8String];
-    NSLog(@"query sql", selectSql);
     if(sqlite3_prepare_v2(database, sqlSentence, -1, &statement, nil) == SQLITE_OK) {
         NSLog(@"query success");
         return 1;
@@ -63,10 +69,13 @@
 
 -(NSArray *) moveToNext {
     NSMutableArray *array;
+    array = [[NSMutableArray alloc] init];
     if(sqlite3_step(statement) == SQLITE_ROW) {
         for(int i=0; i<columnSize; i++) {
+            NSLog(@"moveToNext i=%d", i);
             const unsigned char *result = sqlite3_column_text(statement, i);
-            [array addObject:[[NSString alloc] initWithCString:result]];
+            [array addObject:[NSString stringWithUTF8String:result]];
+            NSLog(@"moveToNext result=%@", [NSString stringWithUTF8String:result]);
         }
         return array;
     } else
